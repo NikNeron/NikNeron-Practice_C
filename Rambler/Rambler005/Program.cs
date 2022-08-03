2b67e34dccd65a5f01aa1f37dc5267bd1f542d2c
NikNeron <nikitaneronov83@gmail.com>
2022-08-04 00:23:53 +0300

    добавили приветствие наташа

---
+++ Rambler/Rambler005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наташа")
+{
+    Console.WriteLine("Ура, это же Наташа!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
