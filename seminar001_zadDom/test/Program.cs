dcf4e6c317f230a91f90c0e6069791763dc77bf6
NikNeron <nikitaneronov83@gmail.com>
2022-08-08 00:01:07 +0300

    Домашка по первому сеинару

---
+++ seminar001_zadDom/test/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+// Первое решение.
+
+if (a > b)
+{
+    Console.Write(a);
+    Console.Write(" больше ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write(b);
+    Console.Write(" больше ");
+    Console.Write(a);
+}
+
+/*Второе решение
+int max = a;
+int min = b;
+if (b > max)
+{
+    max = b;
+}
+if (a < min)
+{
+    min = a;
+}
+
+Console.WriteLine("Выводим большее число");
+Console.WriteLine(max);
+
+Console.WriteLine("Выводим меньшее число");
+Console.WriteLine(min);*/
