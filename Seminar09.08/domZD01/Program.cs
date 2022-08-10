bf01d2274ee21dd5e80dfc180ff86b6943a72890
Елена Иванова <nikitaneronov83@gmail.com>
2022-08-10 19:01:14 +0300

    Домашка по сименару от 9 августа

---
+++ Seminar09.08/domZD01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+
+// 1 вариант
+
+string str = "158";
+Console.WriteLine(str[1]);
+
+// 2 вариант
+Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+
+
+
+
+   
