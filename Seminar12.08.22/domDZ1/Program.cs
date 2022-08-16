568317f655db4156a1c8cc58cf279afd3a3b1389
NikNeron <109685307+NikNeron@users.noreply.github.com>
2022-08-16 11:45:21 +0300

    Update Program.cs

--- Seminar12.08.22/domDZ1/Program.cs
+++ Seminar12.08.22/domDZ1/Program.cs
@@ -4,8 +4,10 @@
 Console.Write("Введите число: ");
 string? number = Console.ReadLine();
 
-void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] && number[1]==number[3])
+  {
     Console.WriteLine($"Ваше число: {number} - ДА.");
   }
   else Console.WriteLine($"Ваше число: {number} - НЕТ.");
