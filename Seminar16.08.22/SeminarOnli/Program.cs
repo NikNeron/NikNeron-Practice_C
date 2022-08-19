700113c0a580713ea25ee4f1f814283991b36aba
NikNeron <109685307+NikNeron@users.noreply.github.com>
2022-08-19 14:53:11 +0300

    Add files via upload

---
+++ Seminar16.08.22/SeminarOnli/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.Readline());
+int summ (int n)
+{
+  int n = 0;
+  for (int i = 1; i <= n; i++)
+  {
+    summ = i+ summ;
+  }
+return (summ);
+}
+
---
+++ Seminar16.08.22/domDZ1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4-> 16 */
+
+int a = 0;
+int b = 0;
+void Enter()
+{
+    Console.WriteLine("Enter the first number: ");
+    a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter the second number: ");
+    b = int.Parse(Console.ReadLine());
+}
+double Pow(int a, int b)
+{
+    return Math.Pow(a, b);
+}
+for (int i = 0; i < 1; i++)
+{
+    Enter();
+    Console.WriteLine(Pow(a, b));
+}
