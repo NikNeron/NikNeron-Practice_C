1023edcf993f901b3ff6ebf92ae2782fc7989c03
NikNeron <nikitaneronov83@gmail.com>
2022-08-25 13:34:26 +0300

    3

---
+++ Seminar22.08.22/domDZ01/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Вводим 3 числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int Mnumber(int a, int b, int c)
+{
+int[] array = new int[3]{a, b, c};
+int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+    {
+        count++; 
+    }
+  }
+return count;
+}
+
+Console.Write($"{Mnumber(a,b,c)} Числa больше ноля");
+
---
+++ Seminar22.08.22/domDZ03/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Доп. задча: Задайте двумерный массив со случайными числами от -10 до 10. 
+Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.)
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12*/
+
+int[,] array = new int[3, 4];
+
+int Sum1(int[,] array)
+{
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+            Console.Write(array[i, j] + " ");
+
+        }
+        Console.WriteLine();
+    }
+    int sum = (array[0, 0] + array[1, 1] + array[2, 2]);
+    return sum;
+}
+int sum = Sum1(array); 
+Console.WriteLine(sum);
