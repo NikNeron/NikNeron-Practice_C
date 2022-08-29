ec6500ddc760e20df95daaf56aae82650a36253d
NikNeron <nikitaneronov83@gmail.com>
2022-08-29 13:13:15 +0300

    добавили пример массивов

---
+++ Lecture/Examplels_Functions/Program.cs
@@ -0,0 +1,119 @@
+﻿//Вид 1
+/*
+void Method1() // где Metod1, является идентификатором метода.
+
+{
+    System.Console.WriteLine("Автор...");
+}
+Method1();
+
+//Вид 2
+//Это метод, которые ничего не возвращают, но в то же время могут принимать какие-то аргументы.
+void Method2(string msq)
+{
+    Console.WriteLine(msq);
+}
+Method2(msq: "Текст сообщения");
+
+////////////////////////////////
+
+void Method21(string msq, int msq)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msq);
+        i++;
+    }
+}
+//Method2(msq:"Текст", count: 4);      
+Method21(count: 4, msq: "сообщения"); //  метод вызывает Текст, после запятой указано количество вызовов, в нашем случае 4.
+
+
+// Вид 3
+//Эти методы, которые что-то возвращают, но ничего не принимают. 
+
+
+int Method3()   //не принимает никакие аргументы
+{
+    return DateTime.Now.Year;   //обязательное использование оператора return,
+}
+
+int year = Method3();  //вызываем метод
+Console.WriteLine(year); //кладём нужное значение
+
+
+// Вид 4
+//Самая важная группа методов, это методы, которые что-то принимают и что-то возвращают
+//Возвращать будем строку string
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;  //string result. Изначально можно и нужно в неё положит какое-то значение.
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;              //Обязательно не забываем увеличивать счётчик и класть в result = result + text (это строчка, которую мы указали).
+    }
+    return result;
+}
+
+string res = Method4(10, " z");
+Console.WriteLine(res);
+
+
+//Цикл for
+
+string Metod4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++) //- вначале ключевое слово, затем инициализация счётчика, после проверка условия и инкремент (увеличение счётчика).
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Metod4(10, "z");
+Console.WriteLine(res);
+
+//Цикл в цикле
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");  // -  это интерполяцию строк
+    }
+    Console.WriteLine();
+}
+*/
+//////////////////////////////////////////////////////
+//Тренировочная задача
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+             + "вы бы взяли приступом согласие прусского короля."
+             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+// 012345   
+
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;  //Напоминаю, что инициализация пустой строки выглядит как string result = String.Empty
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+//string newText = Replace(text, " ", " - ");
+//Console.WriteLine(newText);
+
+
+
---
+++ Lecture/Examplels_array/Program.cs
@@ -0,0 +1,182 @@
+﻿
+//Упорядочить массив
+
+/*
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+           
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+*/
+
+// Двухмерные массивы
+
+/*
+string[,] table = new string[2,5];
+table[1, 2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+      System.Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+*/
+/*
+int[,] matrix = new int[3, 4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+*/
+
+
+/*
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)   //Имя массива.GetLength(0) возвращает количество строк в вашем массиве. 
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)  // Имя массива.GetLength(1) — возвращает количество столбцов.
+        {
+            matr[i, j] = new Random().Next(1, 10);
+
+        }
+
+    }
+}
+
+int[,] matrix = new int[3, 4];
+//PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+*/
+
+/*Мы попробовали использовать двумерные массивы. Теперь важно запомнить, как и что описывается.
+Обязательны:
+● тип данных,
+● квадратные скобки с запятой внутри,
+● именование массива или идентификатор массива,
+● оператор присваивания,
+● обязательное условие new,
+● тип данных,
+● количество строк и столбцов в квадратных скобках. */
+
+
+
+int[,] pic = new int[,]
+{
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+/*
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+    for (int j = 0; j < image.GetLength(1); j++)
+    {
+        if (image[i, j] == 0) Console.Write($" ");
+        else Console.Write($"+");
+        
+
+    }
+    Console.WriteLine();
+}
+}
+
+PrintImage(pic);*/
+
+
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            if (image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)              //Издесь мы определяем правило — что за чем идёт. Сначала поднимаемся на строчку выше (row-1, col),
+                                         //потом идём влево (row, col-1), потом вниз (row+1, col), потом вправо (row, col+1)
+    {
+        pic[row, col] = 1;           
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
+
+//один из алгоритмов, позволяющий закрашивать замкнутые области.
