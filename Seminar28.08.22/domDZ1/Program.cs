afb804e44517707997e4853a68189d965beb452f
NikNeron <nikitaneronov83@gmail.com>
2022-09-01 11:32:36 +0300

    добавил домашку 28.08

---
+++ Seminar28.08.22/domDZ1/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+void PrintArray(int[,] array, bool fillArray)
+    {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+             for (int  j = 0; j < array.GetLength(1); j++)
+             {
+               if(fillArray)
+                array[i,j] = new Random().Next(1,10);
+                Console.Write(array[i,j]+" ");
+             }
+             Console.WriteLine();
+            }
+    }
+        Console.WriteLine("Введите значение для квадратной матрицы ");   
+        // получилось только для квадратной - Как сделать сортировку  для  любой матрицы
+        int x = int.Parse(Console.ReadLine());
+        int[,] array = new int[x,x];
+        PrintArray(array, true);
+
+        int[,] b = new int[x, x];
+        int[] temp = new int[x];
+
+        Console.WriteLine("\nСортировка по строкам: ");
+        for (int i = 0; i < x; i++)
+        {
+          for (int j = 0; j < x; j++)
+                temp[j] = array[i, j];
+
+            Array.Sort(temp);
+              Array.Reverse(temp);
+            for (int k = 0; k < x; k++)
+            {
+                array[i, k] = temp[k];
+            }
+        }
+
+        PrintArray(array, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Seminar28.08.22/domDZ3/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+void PrintArray(int[,] array, bool fillArray)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (fillArray)
+                array[i, j] = new Random().Next(1, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiMatrix(int[,] array1, int[,] array2)
+{
+    int[,] temp = new int[array1.GetLength(0), array2.GetLength(1)];
+
+    for (int i = 0; i < array1.GetLength(0); ++i)
+        for (int j = 0; j < array2.GetLength(0); ++j)
+            for (int k = 0; k < array2.GetLength(1); ++k)
+                temp[i, k] += array1[i, j] * array2[j, k];
+    return temp;
+}
+
+int[,] array1 = { { 2, 4 }, { 3, 2 } };
+int[,] array2 = { { 3, 4 }, { 3, 3 } };
+
+int[,] multiArray = MultiMatrix(array1, array2);
+
+PrintArray(multiArray, false);
